29fb8b44bfc3d4b9593b90fc9fcf0800bc8c908f
ashestoapples <bill.wetherill.3@gmail.com>
2017-07-11 22:23:05 -0400

    keyboard macros

---
+++ keyboard.h
@@ -0,0 +1,32 @@
+#ifndef KEYBOARD_H_INCLUDED
+#define KEYBOARD_H
+
+//top 2 rows of the keyboard
+#define B1 	49
+#define B2 	113
+#define B3 	50  
+#define B4 	119
+#define B5 	51
+#define B6 	101
+#define B7 	52
+#define B8 	114
+#define B9 	53
+#define B10	116
+#define B11	54
+#define	B12 121
+#define B13 55
+#define B14 117
+#define B15 56
+#define B16 105
+
+#define PLAY 	32	//space
+#define PAUSE 	32	//space
+#define COPY	99	//c
+#define PASTE	118	//v
+#define YES		13	//enter
+#define NO		8	//backspace
+#define DELETE	27	//esc
+#define DEMOPLAY 9	//tab
+
+#endif
+
